ebeb365b5b3da3384e8ffd5d0329865f337cacc8
Marcin Kornek <marcin.kornek@netguru.com>
2023-08-05 10:14:30 +0200

    Add module resolver plugin

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: [
+          '.ios.js',
+          '.ios.ts',
+          '.ios.tsx',
+          '.android.js',
+          '.android.ts',
+          '.android.tsx',
+          '.js',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+      },
+    ],
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
+      },
+    ],
+  ],
 };
